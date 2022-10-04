632db0707ad2dd3b5a102c2d223c9e9d6f4f1a10
Margarita P <rprivalko@gmail.com>
2022-10-04 21:16:05 +0300

    hw3 commit

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {Dispatch, useState} from 'react'
 import { v1 } from 'uuid'
 import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
@@ -23,7 +23,7 @@ export type UserType = {
     name: string // need to fix any
 }
 
-export const pureAddUserCallback = (name: string, setUsers: any, users: any) => { // need to fix any
+export const pureAddUserCallback = (name: string, setUsers: Dispatch<UserType[]>, users: UserType[]) => { // need to fix any
     const user = {
         _id: v1(),
         name: name,
