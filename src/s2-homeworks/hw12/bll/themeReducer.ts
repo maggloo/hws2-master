8c21977cdb0350ae15fbd3b7123fbf93bc42d551
Margarita P <rprivalko@gmail.com>
2022-12-01 17:17:27 +0300

    hw12 commit

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,21 @@
+export type ThemeSateType = {
+    themeId: number
+}
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ActionType): ThemeSateType => { // fix any
     switch (action.type) {
-        // дописать
+        case "SET_THEME_ID":
+            return {...state, themeId: action.id}
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+
+type ActionType = ReturnType<typeof changeThemeId>;
+export const changeThemeId = (id: number): { type: 'SET_THEME_ID', id: number } => ({type: 'SET_THEME_ID', id} as const) // fix any
